084f737e48195d688ab0c666d13474dfb90c4357
DevMarco <marcoricovaladez172@gmail.com>
2025-02-25 22:15:50 -0600

    Migrando el sitio web a REACT

---
+++ MERN - TIENDA EN LINEA/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/header'
+import MenuCarrito from './components/MenuCarrito'
+import './App.css'
+import './../public/css/Header.css'
+import './../public/css/Carrito.css'
+
+function App() {
+
+
+
+  return (
+    <>
+      <Header/>
+      <MenuCarrito/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ MERN - TIENDA EN LINEA/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+
+function Header(){
+
+    function handleClick(){
+        console.log("Abriendo el contenido del carrito");
+    }
+
+    return (
+        <>
+            <div className="sec-header">
+                <div id="btn-menu" className="header-items">
+                    <div className="btn-item-menu"></div>
+                    <div className="btn-item-menu"></div>
+                    <div className="btn-item-menu"></div>                
+                </div>
+                <div className="header-items titulo-inicio">Building Technology</div>
+                <div className="header-items">
+                    <form className="form-search navegacion-search" action="" method="post">
+                        <input type="text" className="barra-busqueda" name="buscar" placeholder="Buscar componentes" />    
+                        <button type="submit" className="btn-buscar" name="submits"><img src="./../public/img/simbolo-de-la-interfaz-de-busqueda.png" alt="Error 404" title="Buscar" /></button>
+                    </form>
+                </div>
+                <div id="contenedorCarrito" onClick={() => handleClick()} className="header-items btn-img"><i className="fa-solid fa-cart-shopping"></i></div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
