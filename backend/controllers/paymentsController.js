8ca976301a67ab9e0c7996c0120cc350f9e14cbb
mvaladez <marco.valadez@chenson.com.mx>
2026-05-26 22:59:30 -0600

    Fix page of redirect

--- backend/controllers/paymentsController.js
+++ backend/controllers/paymentsController.js
@@ -130,8 +130,8 @@ const checkPreviousPayment = async (req, res) => {
         payment_method_types: ['card'],
         line_items,
         mode: 'payment',
-        success_url: `http://localhost:5173/Payment/Success/{CHECKOUT_SESSION_ID}`,
-        cancel_url: `http://localhost:5173/Payment/Failure`,
+        success_url: `${process.env.DOMAIN}/Payment/Success/{CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.DOMAIN}/Payment/Failure`,
         metadata: {
             userId,
             cartId,
